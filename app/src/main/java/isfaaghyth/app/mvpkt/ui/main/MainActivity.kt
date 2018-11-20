f00c3463010a442ff34960d05bfc146ac81d64bc
Muh.Isfahani Ghiyath.YM <isfaaghyth@users.noreply.github.com>
2018-11-21 01:44:30 +0700

    fix dependency error for android support

--- app/src/main/java/isfaaghyth/app/mvpkt/ui/main/MainActivity.kt
+++ app/src/main/java/isfaaghyth/app/mvpkt/ui/main/MainActivity.kt
@@ -13,11 +13,11 @@ class MainActivity: BaseActivity<MainPresenter>(), MainView {
     override fun contentView(): Int = R.layout.activity_main
 
     override fun presenter(): MainPresenter {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return MainPresenter()
     }
 
     override fun onCreated(state: Bundle?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
 }
